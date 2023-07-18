a0d23073e3c6f464310f6f6f9c512a6804fc0ba3
jdjdkkdkjjsjs <131205809+jdjdkkdkjjsjs@users.noreply.github.com>
2023-07-18 16:31:04 +0530

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,16 +10,6 @@ export const registerController = async (req, res) => {
     if (!password) {
       return res.send({ message: "Password is Required" });
     }
-    
-    // Check user
-    const existingUser = await userModel.findOne();
-    // Existing user
-    if (existingUser) {
-      return res.status(200).send({
-        success: false,
-        message: "Already registered, please login",
-      });
-    }
 
     // Register user
     const user = await new userModel({
@@ -37,40 +27,6 @@ export const registerController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error",
-      error,
-    });
-  }
-};
-
-export const updateProfileController = async (req, res) => {
-  try {
-    const { name, password, address, phone } = req.body;
-    const user = await userModel.findById(req.user._id);
-    // Password validation
-    if (password && password.length < 6) {
-      return res.json({ error: "Password is required and must be at least 6 characters long" });
-    }
-    const hashedPassword = password ? await hashPassword(password) : undefined;
-    const updatedUser = await userModel.findByIdAndUpdate(
-      req.user._id,
-      {
-        name: name || user.name,
-        password: hashedPassword || user.password,
-        phone: phone || user.phone,
-        address: address || user.address,
-      },
-      { new: true }
-    );
-    res.status(200).send({
-      success: true,
-      message: "Profile Updated Successfully",
-      updatedUser,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(400).send({
-      success: false,
-      message: "Error While Updating Profile",
       error,
     });
   }
