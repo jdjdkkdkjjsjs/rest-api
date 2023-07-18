b42210a41140a62ab1846a5801ee02998f1b9c19
jdjdkkdkjjsjs <131205809+jdjdkkdkjjsjs@users.noreply.github.com>
2023-07-18 05:53:39 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -2,13 +2,9 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-    },
     email: {
       type: String,
+      required: true,
       unique: true,
     },
     password: {
