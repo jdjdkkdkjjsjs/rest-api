f5fbc9a533880002eab2c92a79498905192b9d74
jdjdkkdkjjsjs <131205809+jdjdkkdkjjsjs@users.noreply.github.com>
2023-07-18 16:04:00 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -7,6 +7,9 @@ const userSchema = new mongoose.Schema(
       required: true,
       trim: true,
     },
+    email: {
+      type: String,
+    },
     password: {
       type: String,
       required: true,
